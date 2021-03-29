404ec6f44491ba8861f9c5b095f328adc3a5a89f
Tariq yahya <tariq.yahya300@gmail.com>
2021-03-29 18:27:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const menu =document.querySelector("#mobile-menu");
 const nav__links = document.querySelector(".nav__links--container");
-
+const body = document.querySelector('body')
 
 menu.addEventListener("click", function(){
   nav__links.classList.toggle('active');
   menu.classList.toggle('is-active');
 
+
+
 });
+
+nav__links.addEventListener("click", function(){
+
+
+	nav__links.classList.toggle('active');
+	  menu.classList.toggle('is-active');
+});
+
